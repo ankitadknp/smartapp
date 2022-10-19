76f96eab20f9f28946768481972d3d30a7dc5289
priyankkp-1 <priyank@knp-tech.com>
2022-10-19 19:45:12 +0530

    User management

--- app/PublicFeed.php
+++ app/PublicFeed.php
@@ -12,7 +12,7 @@ class PublicFeed extends Model
     protected $primaryKey = 'public_feed_id';
 
     protected $fillable = [
-        'public_feed_title','content','status','public_feed_title_ab','public_feed_title_he','content_ab','content_he'
+        'public_feed_title','content','status','public_feed_title_ab','public_feed_title_he','content_ab','content_he','short_content','short_content_ab','short_content_he'
     ];
 
     public function images()
