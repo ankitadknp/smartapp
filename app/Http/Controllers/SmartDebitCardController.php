ad409a9f89911670dcd7251ff5812c40d9696ea8
Pradip Rathod <pradipr.knp@gmail.com>
2022-10-20 12:55:57 +0530

    Smart Card Module

---
+++ app/Http/Controllers/SmartDebitCardController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\CouponQRcode;
+use Hash,Redirect,Response,DB,Validator;
+use Illuminate\Validation\Rule;
+use File;
+
+class SmartDebitCardController extends Controller {
+    
+   /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        return view("smart_debit_card.index");
+    }
+
+    public function load_data_in_table(Request $request) {
+
+        $page = !empty($request->get("start")) ? $request->get("start") : 0;
+        $rows = !empty($request->get("length")) ? $request->get("length") : 10;
+        $draw = !empty($request->get("draw")) ? $request->get("draw") : 1;
+
+        $sidx = !empty($request->get("order")[0]['column']) ? $request->get("order")[0]['column'] : 0;
+        $sord = !empty($request->get("order")[0]['dir']) ? $request->get("order")[0]['dir'] : 'DESC';
+
+        $coupon_code = !empty($request->get("coupon_code")) ? $request->get("coupon_code") : '';
+        $coupon_title = !empty($request->get("coupon_title")) ? $request->get("coupon_title") : '';
+
+        $sidx = 'coupon_qrcode.id';
+
+        $list_query = CouponQRcode::select('coupon_qrcode.id', 'coupon_qrcode.qrcode_url', 'coupon_qrcode.qrcode_file', 'coupon.coupon_code', 'coupon.coupon_title')
+                      ->leftjoin('coupon', 'coupon.coupon_id', 'coupon_qrcode.coupon_id')
+                      ->orderBy($sidx, $sord)
+                      ->take($rows);
+
+        if (!empty($coupon_code) ) {
+            $list_query = $list_query->where("coupon.coupon_code", "LIKE", "%" . $coupon_code . "%");
+        }
+        if (!empty($coupon_title)) {
+            $list_query = $list_query->where("coupon.coupon_title", "LIKE", "%" . $coupon_title . "%");
+        }
+
+        $list_query = $list_query->get();
+
+        $total_rows = $list_query->count();
+        $all_records = array();
+
+        if ($total_rows > 0) 
+        {
+            // $list_of_all_data = $list_query->skip($page)
+            //         ->orderBy($sidx, $sord)
+            //         ->take($rows)
+            //         ->get();
+            $index = 0;
+
+            foreach ($list_query as $value) {
+                $all_records[$index]['qrcode_url'] = $value->qrcode_url;
+                $all_records[$index]['qrcode_file'] = $value->qrcode_file ? '<img src="'.$value->qrcode_file.'" width="50px" />' : 'No Image Found';
+                $all_records[$index]['coupon_code'] = $value->coupon_code;
+                $all_records[$index]['coupon_title'] = $value->coupon_title;
+                $all_records[$index]['delete'] = '<button type="button" class="btn btn-danger delete_data_button" data-id="'. $value->id.'">Delete</button>';
+                $index++;
+            }
+        }
+        $response = array();
+        $response['draw'] = (int) $draw;
+        $response['recordsTotal'] = (int) $total_rows;
+        $response['recordsFiltered'] = (int) $total_rows;
+        $response['data'] = $all_records;
+
+        return $response;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request) 
+    {
+        $id = $request->get("id");
+        $find_record = CouponQRcode::find($id);
+
+        if ($find_record) {
+            $destinationPath = public_path("/");
+            $new_path = substr($find_record->qrcode_file, strpos($find_record->qrcode_file, "qrcodes/") );  
+            $image_path = $destinationPath.$new_path;
+            if (File::exists($image_path)) {
+                unlink($image_path);
+            }
+            $find_record->delete();
+            $response['success'] = true;
+            $response['message'] = "QR Code deleted successfully";
+        } else {
+            $response = array("success" => false, "message" => "Problem while deleting this record");
+        }
+
+        return $response;
+    }
+}
---
+++ resources/views/smart_debit_card/index.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.layout')
+
+@section('title', 'Language')
+
+@section('addcss')
+<link rel="stylesheet" href="{{asset("public/assets/modules/datatables/datatables.min.css")}}">
+<link rel="stylesheet" href="{{asset("public/assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css")}}">
+<link rel="stylesheet" href="{{asset("public/assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css")}}">
+<style>
+
+#saveBtn:disabled {
+  background: #999;
+  color: #555;
+  cursor: not-allowed;
+  border: 1px solid #999999;
+}
+
+</style>
+@endsection
+
+@section('content')
+
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <div class="col-lg-12">
+                @php
+                $routeName = explode('.', \Request::route()->getName());
+                @endphp
+                <h1>Coupons QR Management</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body" style="padding: 0px">
+                        <div id="accordion">
+                            <div class="accordion">
+                                <div class="accordion-header collapsed" role="button" data-toggle="collapse" data-target="#panel-body-1" aria-expanded="false">
+                                    <h4>Filter</h4>
+                                </div>
+                                <div class="accordion-body collapse" id="panel-body-1" data-parent="#accordion">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                            <div class="form-group">
+                                                <input type="text" placeholder="Coupon Code" name="coupon_code" id="coupon_code" class="form-control">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                            <div class="form-group">
+                                                <input type="text" placeholder="Coupon Title" name="coupon_title" id="coupon_title" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="col-lg-12">
+                                            <input type="button" name="resetfilter" value="Reset Filter" id="reset-filter" class="btn btn-light float-right reset_filter">
+                                            <input type="button" name="filter" value="Filter" id="apply-filter" class="btn btn-primary float-right search_filter" style="margin-right: 15px;">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped" id="datatable">
+                                <thead>
+                                    <tr>
+                                        <th>Coupon Code</th>
+                                        <th>Coupon Title</th>
+                                        <th>Qrcode Url</th>
+                                        <th>Qrcode</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+@endsection
+
+@section('addjs')
+<script src="{{asset("public/assets/modules/datatables/datatables.min.js")}}"></script>
+<script src="{{asset("public/assets/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js")}}"></script>
+<script src="{{asset("public/assets/modules/datatables/Select-1.2.4/js/dataTables.select.min.js")}}"></script>
+<script src="{{asset("public/assets/modules/jquery-ui/jquery-ui.min.js")}}"></script>
+
+<script type="text/javascript">
+    var controller_url = "{{route('coupons-qr.index')}}";
+</script>
+
+
+<!-- Page Specific JS File -->
+<script src="{{asset("public/assets/pages-js/coupon_qrcode/index.js")}}"></script>
+@endsection
