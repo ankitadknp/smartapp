c549e61215fa308f6967850c5532500a1d89ff98
Pradip Rathod <pradipr.knp@gmail.com>
2022-11-09 14:32:08 +0530

    Remove Duplicate Files

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -53,6 +53,7 @@
                 </a>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <a href="{{route('client.index')}}">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-warning">
                         <i class="fas fa-users"></i>
@@ -66,6 +67,7 @@
                         </div>
                     </div>
                 </div>
+                </a>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <a href="{{route('sub_admin.index')}}">
